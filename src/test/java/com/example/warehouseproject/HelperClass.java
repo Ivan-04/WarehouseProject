5aea0dda786fd35693be09629216637c08e3bcfe
Asus <i.t.nachkov@abv.bg>
2024-12-27 01:44:07 +0200

    All find tests for UserService are created. The only method which is not tested there is createUser(). For this purpose I have created these new classes HelperClass.java and UserServiceImplTests.java.

---
+++ src/test/java/com/example/warehouseproject/HelperClass.java
@@ -0,0 +1,38 @@
+package com.example.warehouseproject;
+
+import com.example.warehouseproject.models.Role;
+import com.example.warehouseproject.models.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class HelperClass {
+
+
+    public static ObjectMapper objectMapper = new ObjectMapper();
+
+
+    public static User createMockUserUser(){
+        var mockUser = new User();
+
+        mockUser.setUserId(1);
+        mockUser.setFirstName("Peter");
+        mockUser.setLastName("Jackson");
+        mockUser.setEmail("ppp.jjj@example.com");
+        mockUser.setPassword("15975248653");
+        mockUser.setRole(Role.OPERATOR);
+
+        return mockUser;
+    }
+
+    public static User createMockUserOwner(){
+        var mockUser = new User();
+
+        mockUser.setUserId(2);
+        mockUser.setFirstName("Gogo");
+        mockUser.setLastName("Tomov");
+        mockUser.setEmail("ggg.ttt@example.com");
+        mockUser.setPassword("15975253");
+        mockUser.setRole(Role.OWNER);
+
+        return mockUser;
+    }
+}
---
+++ src/test/java/com/example/warehouseproject/UserServiceImplTests/UserServiceImplTests.java
@@ -0,0 +1,142 @@
+package com.example.warehouseproject.UserServiceImplTests;
+
+import com.example.warehouseproject.HelperClass;
+import com.example.warehouseproject.exceptions.EntityNotFoundException;
+import com.example.warehouseproject.models.User;
+import com.example.warehouseproject.models.dtos.UserOutput;
+import com.example.warehouseproject.repositories.UserRepository;
+import com.example.warehouseproject.services.UserServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.convert.ConversionService;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTests {
+
+    @Mock
+    UserRepository mockRepository;
+
+    @Mock
+    ConversionService conversionService;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    @Test
+    public void get_Should_Return_AllUsers_WhenOptionsAreFulfilled(){
+        User testUser = HelperClass.createMockUserOwner();
+
+        UserOutput testUserOutput = new UserOutput();
+        testUserOutput.setEmail("ppp.jjj@example.com");
+
+        Mockito.when(conversionService.convert(testUser, UserOutput.class)).thenReturn(testUserOutput);
+        Mockito.when(mockRepository.findAll()).thenReturn(List.of(testUser));
+
+        List<UserOutput> result = userService.findAll();
+        assertEquals(1, result.size());
+        assertEquals("ppp.jjj@example.com", result.get(0).getEmail());
+    }
+
+
+    @Test
+    public void get_Should_Return_CurrentUser_WhenOptionsAreFulfilled(){
+        User testUser = HelperClass.createMockUserUser();
+
+        UserOutput testUserOutput = new UserOutput();
+        testUserOutput.setEmail("ppp.jjj@example.com");
+
+        Mockito.when(conversionService.convert(testUser, UserOutput.class)).thenReturn(testUserOutput);
+        Mockito.when(mockRepository.findUserByUserId(1)).thenReturn(Optional.of(testUser));
+
+        UserOutput result = userService.findUserById(1);
+        assertEquals("ppp.jjj@example.com", result.getEmail());
+        assertThrows(EntityNotFoundException.class, () -> userService.findUserById(99));
+    }
+
+    @Test
+    public void get_Should_Return_CurrentUserEntity_WhenOptionsAreFulfilled(){
+        User testUser = HelperClass.createMockUserUser();
+
+
+        Mockito.when(mockRepository.findUserByUserId(1)).thenReturn(Optional.of(testUser));
+
+        User result = userService.findUserEntityById(1);
+        assertEquals("ppp.jjj@example.com", result.getEmail());
+        assertThrows(EntityNotFoundException.class, () -> userService.findUserEntityById(99));
+    }
+
+    @Test
+    public void get_Should_Return_CurrentUser_WhenFirstNameIsFulfilled(){
+        User testUser = HelperClass.createMockUserOwner();
+
+        UserOutput testUserOutput = new UserOutput();
+        testUserOutput.setFirstName("Gogo");
+
+        Mockito.when(conversionService.convert(testUser, UserOutput.class)).thenReturn(testUserOutput);
+        Mockito.when(mockRepository.findUserByFirstName("Gogo")).thenReturn(Optional.of(testUser));
+
+        UserOutput result = userService.findUserByFirstName("Gogo");
+        assertEquals("Gogo", result.getFirstName());
+        assertThrows(EntityNotFoundException.class, () -> userService.findUserByFirstName("Nasko"));
+    }
+
+    @Test
+    public void get_Should_Return_CurrentUser_WhenLastNameIsFulfilled(){
+        User testUser = HelperClass.createMockUserOwner();
+
+        UserOutput testUserOutput = new UserOutput();
+        testUserOutput.setLastName("Tomov");
+
+        Mockito.when(conversionService.convert(testUser, UserOutput.class)).thenReturn(testUserOutput);
+        Mockito.when(mockRepository.findUserByLastName("Tomov")).thenReturn(Optional.of(testUser));
+
+        UserOutput result = userService.findUserByLastName("Tomov");
+        assertEquals("Tomov", result.getLastName());
+        assertThrows(EntityNotFoundException.class, () -> userService.findUserByLastName("Atanasov"));
+    }
+
+    @Test
+    public void get_Should_Return_CurrentUser_WhenEmailIsFulfilled(){
+        User testUser = HelperClass.createMockUserUser();
+
+        UserOutput testUserOutput = new UserOutput();
+        testUserOutput.setEmail("ppp.jjj@example.com");
+
+        Mockito.when(conversionService.convert(testUser, UserOutput.class)).thenReturn(testUserOutput);
+        Mockito.when(mockRepository.findUserByEmail("ppp.jjj@example.com")).thenReturn(Optional.of(testUser));
+
+        UserOutput result = userService.findUserByEmail("ppp.jjj@example.com");
+        assertEquals("ppp.jjj@example.com", result.getEmail());
+        assertThrows(EntityNotFoundException.class, () -> userService.findUserByEmail("jjj.jjj@example.com"));
+    }
+
+    @Test
+    public void get_Should_Return_CurrentUserEntity_WhenEmailIsFulfilled(){
+        User testUser = HelperClass.createMockUserUser();
+
+        Mockito.when(mockRepository.findUserByEmail("ppp.jjj@example.com")).thenReturn(Optional.of(testUser));
+
+        User result = userService.findUserEntityByEmail("ppp.jjj@example.com");
+        assertEquals("ppp.jjj@example.com", result.getEmail());
+        assertThrows(EntityNotFoundException.class, () -> userService.findUserEntityByEmail("jjj.jjj@example.com"));
+    }
+
+    @Test
+    public void get_Should_Return_boolean_WhenEmailIsFulfilled(){
+        User testUser = HelperClass.createMockUserUser();
+
+        Mockito.when(mockRepository.existsByEmail("ppp.jjj@example.com")).thenReturn(true);
+
+        boolean result = userService.existsByEmail("ppp.jjj@example.com");
+        assertTrue(result);
+    }
+}
